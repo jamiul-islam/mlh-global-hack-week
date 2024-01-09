51d3bdef505b834ca08479a92c4f71a013aa35b3
JAMIUL ISLAM <60044835+jamiul-islam@users.noreply.github.com>
2024-01-09 17:41:42 +0000

    completed sorting-algorithm

---
+++ sorting-method.js
@@ -0,0 +1,19 @@
+// write a function that takes an array of numbers and returns a sorted array using bubble sort
+function bubbleSort(arr) {
+    let sorted = false;
+    while (!sorted) {
+        sorted = true;
+        for (let i = 0; i < arr.length - 1; i++) {
+            let j = i + 1;
+            if (arr[i] > arr[j]) {
+                sorted = false;
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr; 
+ 
+}
+  console.log(bubbleSort([3, 4, 9, 3, 1]));
